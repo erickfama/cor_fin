6ef42bfff8b40135866395a85c2eef0386ffe5b9
Erick Fajardo <erickfama@outlook.es>
2022-07-13 12:31:40 -0500

    Optmizacion F1 score y curvas ROC de egresos e ingreos

--- src/3_ROC.R
+++ src/3_ROC.R
@@ -3,28 +3,62 @@
 # Librerias ----
 library(tidyverse)
 
-# Lectura ----
-roc_og <- read_csv("./data/2_interim/roc_og.csv", locale = locale(encoding = "latin1"))
-roc_per <- read_csv("./data/2_interim/roc_per.csv", locale = locale(encoding = "latin1"))
-roc_inc <- read_csv("./data/2_interim/roc_inc.csv", locale = locale(encoding = "latin1"))
+# Egresos ----
 
-# Plots ----
+## Lectura ----
+egresos_per <- read_csv("./data/3_final/egresos19_per_clean.csv")
+egresos_inc <- read_csv("./data/3_final/egresos19_inc_clean.csv")
 
-# Merge data frames 
-roc <- rbind(roc_og, roc_per, roc_inc)
+## ROC ----
+roc_eg_per <- ROC(egresos_per, method_name = "Percepción", props_values = seq(0, 20, length.out = 21))
+roc_eg_inc <- ROC(egresos_inc, method_name = "Incidencia", props_values = seq(0, 20, length.out = 21))
+roc_eg <- rbind(roc_eg_per, roc_eg_inc)
 
-# plot 
-roc %>%
-  ggplot(aes(recall, precision, color = method)) +
+## Escritura ----
+write_csv(roc_eg, "./data/2_interim/roc_eg.csv")
+
+## plot ----
+roc_eg <- read_csv("./data/2_interim/roc_eg.csv", locale = locale(encoding = "latin1"))
+
+roc_eg %>%
+  ggplot(aes(FPR, TPR, color = method)) +
   geom_point() +
   geom_line() +
-  # ggrepel::geom_text_repel(aes(label = round(prop, 2))) +
-  scale_x_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 5)) +
-  scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 5)) +
+  ggrepel::geom_text_repel(aes(label = round(prop, 2))) +
+  scale_x_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 10)) +
+  scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 10)) +
   scale_color_discrete(name = "Modelo") +
   theme_bw() +
   labs(title = "Curvas ROC",
-       x = "Recall (sensitivity)",
-       y = "Precision",
+       subtitle = "Egresos",
        caption = "Elaboración propia.")
 
+# Ingresos ----
+
+## Lectura ----
+ingresos_per <- read_csv("./data/3_final/ingresos19_per_clean.csv", locale = locale(encoding = "latin1"))
+ingresos_inc <- read_csv("./data/3_final/ingresos19_inc_clean.csv", locale = locale(encoding = "latin1"))
+
+## ROC ----
+roc_ig_per <- ROC(ingresos_per, method_name = "Percepción", props_values = seq(0, 20, length.out = 21))
+roc_ig_inc <- ROC(ingresos_inc, method_name = "Incidencia", props_values = seq(0, 20, length.out = 21))
+roc_ig <- rbind(roc_ig_per, roc_ig_inc)
+
+## Escritura ----
+write_csv(roc_ig, "./data/2_interim/roc_ig.csv")
+
+## plot ----
+roc_ig <- read_csv("./data/2_interim/roc_ig.csv", locale = locale(encoding = "UTF-8"))
+
+roc_ig %>%
+  ggplot(aes(FPR, TPR, color = method)) +
+  geom_point() +
+  geom_line() +
+  ggrepel::geom_text_repel(aes(label = round(prop, 2))) +
+  scale_x_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 10)) +
+  scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 10)) +
+  scale_color_discrete(name = "Modelo") +
+  theme_bw() +
+  labs(title = "Curvas ROC",
+       subtitle = "Ingresos",
+       caption = "Elaboración propia.")
---
+++ src/3_ROC_function.R
@@ -0,0 +1,62 @@
+### ROC function ### 
+
+# Librerias ----
+library(tidyverse)
+library(caret)
+library(xgboost)
+
+# ROC ----
+
+ROC <- function(data, method_name, props_values, seed = TRUE){
+  
+  # Lectura 
+  if(is.character(data) == TRUE){
+    data <- read_csv(data)
+  } else {
+    data <- data
+  }
+  
+  props <- props_values
+  
+  roc <- map_df(props, function(p){
+    
+    # Data set
+    df <- data %>%
+      mutate(across(starts_with("prop"), ~ ifelse(.x > p, 1, 0), .names = "corrup"),
+             corrup = factor(corrup, levels = c(1, 0), labels = c("corrupto", "no_corrupto"))) %>%
+      select(pobtot, graproes, starts_with("partida"), corrup)
+    
+    # Train y test sets ----
+    if(seed == TRUE){
+      set.seed(123)
+    }
+    training_samples <- df$corrup %>% 
+      createDataPartition(p = 0.7, list = FALSE)
+    train_set  <- df[training_samples, ]
+    test_set <- df[-training_samples, ]
+    
+    # xgbTree ----
+    
+    # Training
+    model <- train(corrup ~ ., 
+                   data = train_set, 
+                   method = "xgbTree",
+                   trControl = trainControl("cv", number = 5))
+    
+    # Ajuste con test set
+    y_hat <- model %>% predict(test_set)
+    
+    list(method = method_name,
+         prop = p,
+         prevalence = round(mean(df$corrup == "corrupto"), 4),
+         specifiticy = specificity(y_hat, test_set$corrup),
+         FPR = 1 - specificity(y_hat, test_set$corrup),
+         TPR = sensitivity(y_hat, test_set$corrup),
+         precision = precision(y_hat, test_set$corrup),
+         F1_score = F_meas(y_hat, test_set$corrup))
+  })
+  # Best cutoff
+  # roc$best_cutoff <- roc$prop[which(roc$prevalence == max(roc$prevalence[which(roc$F1_score == max(roc$F1_score[which(roc$recall >= roc$specifiticy)]))]))]
+  beepr::beep(sound = 2)
+  return(roc)
+}
