b29a75483a26eac5b95a23d6c3e2464109cc00f4
Erick Fajardo <erickfama@outlook.es>
2022-08-05 15:08:24 -0500

    Se agregaron los datos del censo a la base final

--- src/2_limpieza_baseFinal.R
+++ src/2_limpieza_baseFinal.R
@@ -102,23 +102,48 @@ efipem19_clean <- read_csv("./data/2_interim/efipem19_clean.csv")
 censo2020_mun_clean <- read_csv("./data/2_interim/censo2020_mun_clean.csv") %>%
   select(mun_inegi, pobtot, pea, graproes)
 
+## Censo economico 2019 ----
+censo_econ2019_clean <- fst::read_fst("./data/2_interim/censo_econ2019_clean.fst")
+
 ## Merge incidencia ----
 efipemCor19_inc_raw <- efipem19_clean %>%
   left_join(encig19_inc_clean, by = "mun_inegi") %>%
   left_join(censo2020_mun_clean, by = "mun_inegi") %>%
+  left_join(censo_econ2019_clean, by = "mun_inegi") %>%
   select(-c("id_entidad", "id_municipio"))
 
 ## Limpieza incidencia ----
 efipemCor19_inc_clean <- efipemCor19_inc_raw %>%
   filter(!is.na(prop_corrup)) %>%
-  select(mun_inegi, nom_ent, nom_mun, prop_corrup, tema, categoria, descripcion_categoria, valor, pobtot, pea, graproes) %>%
+  mutate(estado = substr(mun_inegi, 1, 2)) %>%
+  select(mun_inegi, estado, everything()) %>%
   mutate(across(c("tema", "categoria", "descripcion_categoria"), ~ stri_trans_general(str_replace_all(str_replace_all(str_to_lower(.x), "\\s", "_"), ",", ""), id = "Latin-ASCII")),
          valor = valor/pea) # Gasto per capita PEA
-  
+
+# Municipios sin datos economicos 
+mun_NA <- unique(efipemCor19_inc_clean$mun_inegi[is.na(efipemCor19_inc_clean$a111a_23)])
+mean_mun <- censo_econ2019_clean %>%
+  mutate(estado = substr(mun_inegi, 1, 2)) %>%
+  filter(estado %in% substr(mun_NA, 1, 2)) %>%
+  group_by(estado) %>%
+  summarise(across(everything(), mean)) %>%
+  select(-mun_inegi)
+
+# Se agregan las medias estatales a los municipios con NA
+
+for(muni in mun_NA){
+  index <- which(efipemCor19_inc_clean$mun_inegi == muni)
+  index_mean <- which(mean_mun$estado == substr(muni, 1, 2))
+  for(var in select(efipemCor19_inc_clean, starts_with(c("a", "h", "j", "k"))) %>% names()){
+    efipemCor19_inc_clean[index, var] <- mean_mun[index_mean, var]
+  }
+}
+efipemCor19_clean <- efipemCor19_inc_clean %>%
+  select(-estado)
 
 ## Escritura base completa incidencia ----
-write.csv(efipemCor19_inc_raw, "./data/2_interim/efipemCor19_inc_raw.csv")
-write.csv(efipemCor19_inc_clean, "./data/2_interim/efipemCor19_inc_clean.csv")
+fst::write_fst(efipemCor19_inc_raw, "./data/2_interim/efipemCor19_inc_raw.fst")
+fst::write_fst(efipemCor19_inc_clean, "./data/2_interim/efipemCor19_inc_clean.fst")
 
 # Base Egresos ----
 egresos19_inc_clean <- efipemCor19_inc_clean %>%
@@ -135,5 +160,5 @@ ingresos19_inc_clean <- efipemCor19_inc_clean %>%
   select(-pea)
 
 # Escritura ingresos/egresos ----
-write_csv(egresos19_inc_clean, "./data/3_final/egresos19_inc_clean.csv")
-write_csv(ingresos19_inc_clean, "./data/3_final/ingresos19_inc_clean.csv")
+fst::write_fst(egresos19_inc_clean, "./data/3_final/egresos19_inc_clean.fst")
+fst::write_fst(ingresos19_inc_clean, "./data/3_final/ingresos19_inc_clean.fst")
--- src/2_limpieza_censoEconomico2019.R
+++ src/2_limpieza_censoEconomico2019.R
@@ -1,5 +1,4 @@
 ### Limpieza Censo Economico 2019 ###
-eeric1kdodo
 
 # Librerias -----
 library(tidyverse)
@@ -20,6 +19,8 @@ abreviaturas <- url_abrevs %>%
   replace(list = c(15, 23), values = c("mex", "qroo"))
 
 # Descarga ----
+
+## Censo economico ----
 
 # Urls 
 url_base <- "https://www.inegi.org.mx/contenidos/programas/ce/2019/Datosabiertos/ce2019_ags_csv.zip"
@@ -43,6 +44,13 @@ censo_estados <- purrr::map2(url_estados, csv_files, function(url, csv){
 censo_econ_estados <- map_dfr(censo_estados, ~ .x %>%
                                 mutate(across(everything(), as.character)))
 
+## Descriptores identificadores actividades economicas ----
+
+id_act <- readxl::read_xlsx("./data/1_raw/scian_2018_categorias_y_productos.xlsx", skip = 1) %>%
+  janitor::clean_names() %>%
+  filter(!is.na(codigo) & codigo < 100) %>%
+  mutate(codigo = as.character(codigo))
+
 # Escritura ----
 write.csv(censo_econ_estados, "./data/1_raw/censo_econ2019_raw.csv", row.names = FALSE)
 fst::write.fst(censo_econ_estados, "./data/1_raw/censo_econ2019_raw.fst")
@@ -54,7 +62,17 @@ censo_econ2019_raw <- fst::read_fst("./data/1_raw/censo_econ2019_raw.fst")
 censo_econ2019_clean <- censo_econ2019_raw %>%
   select(ENTIDAD, MUNICIPIO, CODIGO, ID_ESTRATO, UE, A111A, A211A, A700A,	A800A,	H000D,	H001A,	H010D,	J000A,	J300A,	J500A,	K311A) %>%
   janitor::clean_names() %>%
-  filter(!is.na(municipio))
+  filter(!is.na(municipio) & codigo %in% id_act$codigo & !is.na(a111a)) %>%
+  left_join(select(id_act, codigo, titulo), by = "codigo") %>%
+  mutate(mun_inegi = paste(entidad, municipio, sep = ""),
+         across(c(a111a:k311a), as.numeric)) %>%
+  select(mun_inegi, codigo, titulo, a111a:k311a) %>%
+  group_by(mun_inegi, codigo, titulo) %>%
+  summarise(across(c(a111a:k311a), sum)) %>%
+  pivot_wider(id_cols = mun_inegi, names_from = codigo, values_from = a111a:k311a) %>%
+  mutate(across(1:last_col(), ~ ifelse(is.na(.x) == TRUE, 0, .x)))
+
+rm(censo_econ2019_raw, id_act)
 
 # Escritura ----
 fst::write_fst(censo_econ2019_clean, "./data/2_interim/censo_econ2019_clean.fst")
