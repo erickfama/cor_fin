0c61f5e3ac4468caaee88e84311a1533d6fda60f
Erick Fajardo <erickfama@outlook.es>
2022-09-14 15:16:14 -0500

    Primera version de diccionarios, falta desc finanzas

---
+++ src/2_diccionarios.R
@@ -0,0 +1,120 @@
+### Diccionarios de variables ###
+
+# Librerías ----
+library(tidyverse)
+library(fst)
+
+# Lectura ----
+eg_per_clean_all <- read_fst("./data/3_final/eg_per_all_clean.fst")
+ig_per_clean_all <- read_fst("./data/3_final/ig_per_all_clean.fst")
+# eg_inc_clean_all <- read_fst("./data/3_final/eg_inc_all_clean.fst")
+# ig_inc_clean_all <- read_fst("./data/3_final/ig_inc_all_clean.fst")
+
+# Nombres de variables 
+var_names_eg <- names(eg_per_clean_all) 
+  
+var_names_ig <- names(ig_per_clean_all)
+rm(eg_per_clean_all, ig_per_clean_all)
+
+# Diccionario SCIAN 2018 ----
+scian_dicc <- readxl::read_xlsx("./data/1_raw/scian_2018_categorias_y_productos.xlsx", skip = 1) %>%
+  janitor::clean_names() %>%
+  filter(!is.na(codigo) & codigo < 100) %>%
+  mutate(codigo = as.character(codigo))
+
+# Diccionario Censo económico ----
+ce_dicc <- read_csv("./data/1_raw/diccionario_de_datos_ce2019.csv") %>%
+  janitor::clean_names() %>%
+  mutate(columna = str_to_lower(columna))
+
+# Diccionario Egresos ----
+
+# Variables genéricas
+vars_genericas_per <- c("Clave de municipio (últimos 4 dígitos de la clave INEGI)", "Distinción entre egresos e ingresos", "Nombre del estado al que pertenece el municipio", "Nombre del municipio", "Población total calculada con el factor de expansión que reportó percibir que las prácticas de corrupción son muy frecuentes en su municipio de residencia", "Resto de la población calculada con el factor de expansión que respondió percibir que las prácticas de corrupción son frecuentes hasta poco frecuentes en su municipio de residencia", "Proporción de la población calculada con el factor de expansión que percibe las prácticas de corrupción como muy frecuentes en su municipio de residencia", "Población del municipio con base en el censo de población más cercano (2020 en este caso)", "Grado promedio de escolaridad de la población del municipio con base en el censo de población más cercano (2020 en este caso)")
+vars_genericas_inc <- c("Clave de municipio (últimos 4 dígitos de la clave INEGI)", "Distinción entre egresos e ingresos", "Nombre del estado al que pertenece el municipio", "Nombre del municipio", "Población total calculada con el factor de expansión que reportó haber participado en algún acto de corrupción en los últimos 5 años", "Resto de la población calculada con el factor de expansión que respondió no haber participado en algún acto de corrupción en los últimos 5 años", "Proporción de la población calculada con el factor de expansión que participó en algún acto de corrupción en los últimos 5 años", "Población del municipio con base en el censo de población más cercano (2020 en este caso)", "Grado promedio de escolaridad de la población del municipio con base en el censo de población más cercano (2020 en este caso)")
+
+## Percepción ----
+
+eg_dicc_per <- data.frame(variables = var_names_eg)
+
+eg_dicc_per$descripcion <- c()
+eg_dicc_per[1:9, "descripcion"] <- vars_genericas_per
+
+for(i in ce_dicc$columna){
+  if(i %in% str_extract(eg_dicc_per$variable, "^[a-z][0-9]+[a-z]")){
+    descrip <- ce_dicc[which(ce_dicc$columna == i), "descripcion"]
+    eg_dicc_per[which(str_extract(eg_dicc_per$variable, "^[a-z][0-9]+[a-z]") == i), "descripcion"] <- descrip
+  }
+}
+
+for(i in scian_dicc$codigo){
+  if(i %in% str_extract(eg_dicc_per$variable, "\\d*$")){
+    descrip <- scian_dicc[which(scian_dicc$codigo == i), "titulo"]
+    eg_dicc_per[which(str_extract(eg_dicc_per$variable, "\\d*$") == i), "descripcion"] <- paste(unique(eg_dicc_per[which(str_extract(eg_dicc_per$variable, "\\d*$") == i), "descripcion"]), descrip, sep = ";")
+  }
+}
+
+## Incidencia ----
+
+eg_dicc_inc <- data.frame(variables = var_names_eg)
+
+eg_dicc_inc$descripcion <- c()
+eg_dicc_inc[1:9, "descripcion"] <- vars_genericas_inc
+
+for(i in ce_dicc$columna){
+  if(i %in% str_extract(eg_dicc_inc$variable, "^[a-z][0-9]+[a-z]")){
+    descrip <- ce_dicc[which(ce_dicc$columna == i), "descripcion"]
+    eg_dicc_inc[which(str_extract(eg_dicc_inc$variable, "^[a-z][0-9]+[a-z]") == i), "descripcion"] <- descrip
+  }
+}
+
+for(i in scian_dicc$codigo){
+  if(i %in% str_extract(eg_dicc_inc$variable, "\\d*$")){
+    descrip <- scian_dicc[which(scian_dicc$codigo == i), "titulo"]
+    eg_dicc_inc[which(str_extract(eg_dicc_inc$variable, "\\d*$") == i), "descripcion"] <- paste(unique(eg_dicc_inc[which(str_extract(eg_dicc_inc$variable, "\\d*$") == i), "descripcion"]), descrip, sep = ";")
+  }
+}
+
+# Diccionario Ingresos ----
+
+## Percepción ----
+
+ig_dicc_per <- data.frame(variables = var_names_ig)
+
+ig_dicc_per$descripcion <- c()
+
+ig_dicc_per[1:9, "descripcion"] <- vars_genericas_per
+for(i in ce_dicc$columna){
+  if(i %in% str_extract(ig_dicc_per$variable, "^[a-z][0-9]+[a-z]")){
+    descrip <- ce_dicc[which(ce_dicc$columna == i), "descripcion"]
+    ig_dicc_per[which(str_extract(ig_dicc_per$variable, "^[a-z][0-9]+[a-z]") == i), "descripcion"] <- descrip
+  }
+}
+
+for(i in scian_dicc$codigo){
+  if(i %in% str_extract(ig_dicc_per$variable, "\\d*$")){
+    descrip <- scian_dicc[which(scian_dicc$codigo == i), "titulo"]
+    ig_dicc_per[which(str_extract(ig_dicc_per$variable, "\\d*$") == i), "descripcion"] <- paste(unique(ig_dicc_per[which(str_extract(ig_dicc_per$variable, "\\d*$") == i), "descripcion"]), descrip, sep = ";")
+  }
+}
+
+## Incidencia ----
+
+ig_dicc_inc <- data.frame(variables = var_names_ig)
+
+ig_dicc_inc$descripcion <- c()
+
+ig_dicc_inc[1:9, "descripcion"] <- vars_genericas_inc
+for(i in ce_dicc$columna){
+  if(i %in% str_extract(ig_dicc_inc$variable, "^[a-z][0-9]+[a-z]")){
+    descrip <- ce_dicc[which(ce_dicc$columna == i), "descripcion"]
+    ig_dicc_inc[which(str_extract(ig_dicc_inc$variable, "^[a-z][0-9]+[a-z]") == i), "descripcion"] <- descrip
+  }
+}
+
+for(i in scian_dicc$codigo){
+  if(i %in% str_extract(ig_dicc_inc$variable, "\\d*$")){
+    descrip <- scian_dicc[which(scian_dicc$codigo == i), "titulo"]
+    ig_dicc_inc[which(str_extract(ig_dicc_inc$variable, "\\d*$") == i), "descripcion"] <- paste(unique(ig_dicc_inc[which(str_extract(ig_dicc_inc$variable, "\\d*$") == i), "descripcion"]), descrip, sep = ";")
+  }
+}
