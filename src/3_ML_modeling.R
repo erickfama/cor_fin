6568624bf33d89cc2df649a23c61cc75af1e3bc4
Erick Fajardo <erickfama@outlook.es>
2022-09-20 13:48:04 -0500

    Se eliminan los modelos de percepción

--- src/3_ML_modeling.R
+++ src/3_ML_modeling.R
@@ -8,25 +8,23 @@ library(xgboost)
 source("./src/3_ML_function.R", encoding = "UTF-8")
 
 # Lectura ----
-eg_per <- read_fst("./data/3_final/egresos19_per_clean.fst")
 eg_inc <- read_fst("./data/3_final/egresos19_inc_clean.fst")
 
-ig_per <- read_fst("./data/3_final/ingresos19_per_clean.fst")
 ig_inc <- read_fst("./data/3_final/ingresos19_inc_clean.fst")
 
 # Egresos ----
 
-## Percepcion ----
-model_eg_per <- gb_model(eg_per, cutoff = best_cutoff("eg", "per"))
-
 ## incidencia ----
 model_eg_inc <- gb_model(eg_inc, cutoff = best_cutoff("eg", "inc"))
 
-# Ingresos ----
+# Guardado
+saveRDS(model_eg_inc, file = "./models/model_eg_inc.rds")
 
-## Percepcion ----
-model_ig_per <- gb_model(ig_per, cutoff = best_cutoff("ig", "per"), seed = TRUE)
+# Ingresos ----
 
 ## Incidencia ----
 model_ig_inc <- gb_model(ig_inc, cutoff = best_cutoff("ig", "inc"), seed = TRUE)
+ 
+# Guardado
+saveRDS(model_ig_inc, file = "./models/model_ig_inc.rds")
 
