6723491d6c6543cb17903c5461b816ff0ed98831
Erick Fajardo <erickfama@outlook.es>
2022-09-24 13:05:52 -0500

    Correccion conjuntos de datos para entrenar

--- src/3_ML_modeling.R
+++ src/3_ML_modeling.R
@@ -8,14 +8,16 @@ library(xgboost)
 source("./src/3_ML_function.R", encoding = "UTF-8")
 
 # Lectura ----
-eg_inc <- read_fst("./data/3_final/egresos19_inc_clean.fst")
+eg_inc <- read_fst("./data/3_final/eg_inc_all_clean.fst") %>%
+  filter(!is.na(prop_corrup)) # IMPORTANTE para entrenar
 
-ig_inc <- read_fst("./data/3_final/ingresos19_inc_clean.fst")
+ig_inc <- read_fst("./data/3_final/ig_inc_all_clean.fst") %>%
+  filter(!is.na(prop_corrup))
 
 # Egresos ----
 
 ## incidencia ----
-model_eg_inc <- gb_model(eg_inc, cutoff = best_cutoff("eg", "inc"))
+model_eg_inc <- gb_model(eg_inc, cutoff = best_cutoff("eg", "inc"), seed = TRUE)
 
 # Guardado
 saveRDS(model_eg_inc, file = "./models/model_eg_inc.rds")
