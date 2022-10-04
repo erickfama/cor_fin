1519f7ac2f0fa27d8855d688ee34873a047f5364
Erick Fajardo <erickfama@outlook.es>
2022-10-04 13:59:10 -0500

    Actualización de entrenamiento con RF

--- src/train_test.R
+++ src/train_test.R
@@ -7,7 +7,8 @@ library(randomForest)
 
 # Lectura
 eg_inc <- fst::read_fst("./data/3_final/eg_inc_all_clean.fst") %>%
-  filter(!is.na(prop_corrup))
+  filter(!is.na(prop_corrup)) %>%
+  rename("partida_generica_herramientas_y_maquinas_herramienta" = "partida_generica_herramientas_y_maquinas-herramienta")
 training_samples <- readRDS("./data/2_interim/training_samples.RDS")
 data <- eg_inc
 
@@ -29,19 +30,33 @@ qplot(sds, bins = ncol(df))
 
 
 df <- df[-nearZeroVar(df)]
-sds <- colSds(df %>% as.matrix())
-sds <- sds[-which.max(sds)]
-qplot(sds, bins = ncol(df))
+new_sds <- colSds(df %>% select(-corrup) %>% as.matrix())
+new_sds <- sds[-which.max(sds)]
+qplot(new_sds, bins = ncol(df))
 
 # xgbTree ----
+
+train_set  <- df[training_samples, ] 
+test_set <- df[-training_samples, ]
 
 # Training
 model <- train(corrup ~ ., 
                data = train_set, 
                method = "rf",
-               trControl = trainControl("cv", number = 5, repeats = 5),
-               ntree = 150,
-               nSamp = 5000)
+               trControl = trainControl("repeatedcv", number = 10, repeats = 5),
+               tuneGrid = expand.grid(mtry = c(1, seq(5, 100, 5))))
+
+model$bestTune
+
+# Entrenamiento final
+final_model <- randomForest(corrup ~ .,
+                            data = train_set,
+                            minNode = model$bestTune$mtry)
+
+plot(final_model)
+y_hat <- predict(final_model, test_set)
+final_cm <- confusionMatrix(y_hat, reference = test_set$corrup)
+final_cm
 beepr::beep(sound = 13)
 y_hat <- model %>% predict(test_set)
 ggplot(model, highlight = TRUE)
@@ -59,8 +74,4 @@ y_hat <- fit_rf %>% predict(test_set)
 cm_tree <- confusionMatrix(y_hat, reference = test_set$corrup)
 cm_tree
 
-data(iris)
-set.seed(71)
-iris.rf <- randomForest(Species ~ ., data=iris, importance=TRUE,
-                        minnode = 35,
-                        proximity=TRUE)
+
