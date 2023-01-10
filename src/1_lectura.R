14434c3596cf8618c0efbc45f9af089c1ec9249a
Erick Fajardo <erickfama@outlook.es>
2023-01-10 17:58:22 -0600

    Se añaden los años de 2018, 2019, 2020 y 2021

--- src/1_lectura.R
+++ src/1_lectura.R
@@ -9,12 +9,24 @@ library(tidyverse)
 encig19_raw <- read_csv("./data/1_raw/conjunto_de_datos_encig2019_01_sec1_3_4_5_8_9_10.csv") %>%
   janitor::clean_names()
 
-## Ingresos y egresos muni 2019 ----
+## Ingresos y egresos muni 2018, 2019, 2020, 2021 ----
+efipem18_raw <- read_csv("./data/1_raw/efipem_municipal_anual_tr_cifra_2018.csv") %>%
+  janitor::clean_names()
+
 efipem19_raw <- read_csv("./data/1_raw/efipem_municipal_anual_tr_cifra_2019.csv") %>% 
   janitor::clean_names()
+
+efipem20_raw <- read_csv("./data/1_raw/efipem_municipal_anual_tr_cifra_2020.csv") %>% 
+  janitor::clean_names()
+
+efipem21_raw <- read_csv("./data/1_raw/efipem_municipal_anual_tr_cifra_2021.csv") %>% 
+  janitor::clean_names()
+
+efipem_raw <- bind_rows(efipem18_raw, efipem19_raw, efipem20_raw, efipem21_raw)
 
 # Escritura ----
 write_csv(encig19_raw, "./data/1_raw/encig19_raw.csv")
 write_csv(efipem19_raw, "./data/1_raw/efipem19_raw.csv")
+write_csv(efipem_raw, "./data/1_raw/efipem_raw.csv")
 
 
