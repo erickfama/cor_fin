caab48422ae79bf25d4aa3b33a97340f7a706c0f
Erick Fajardo <erickfama@outlook.es>
2023-03-04 20:05:17 -0600

    Se agregan datos ENCIG 17 y 21

--- src/1_lectura.R
+++ src/1_lectura.R
@@ -5,8 +5,16 @@ library(tidyverse)
 
 # Lectura ----
 
+## Encig 2017 ---- 
+encig17_raw <- read.csv("./data/1_raw/encig2017_01_sec1_3_4_5_8_9_10.csv") %>%
+  janitor::clean_names()
+
 ## Encig 2019 ----
 encig19_raw <- read_csv("./data/1_raw/conjunto_de_datos_encig2019_01_sec1_3_4_5_8_9_10.csv") %>%
+  janitor::clean_names()
+
+## Encig 2021 ----
+encig21_raw <- read.csv("./data/1_raw/conjunto_de_datos_encig2021_01_sec1_A_3_4_5_8_9_10.csv") %>%
   janitor::clean_names()
 
 ## Ingresos y egresos muni 2018, 2019, 2020, 2021 ----
@@ -25,8 +33,9 @@ efipem21_raw <- read_csv("./data/1_raw/efipem_municipal_anual_tr_cifra_2021.csv"
 efipem_raw <- bind_rows(efipem18_raw, efipem19_raw, efipem20_raw, efipem21_raw)
 
 # Escritura ----
+write.csv(encig17_raw, "./data/1_raw/encig17_raw.csv", row.names = FALSE)
 write_csv(encig19_raw, "./data/1_raw/encig19_raw.csv")
+write.csv(encig21_raw, "./data/1_raw/encig21_raw.csv", row.names = FALSE)
 write_csv(efipem19_raw, "./data/1_raw/efipem19_raw.csv")
 write_csv(efipem_raw, "./data/1_raw/efipem_raw.csv")
-
 
